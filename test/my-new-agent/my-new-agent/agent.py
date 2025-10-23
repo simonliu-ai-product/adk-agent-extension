92a1c98e0fd98ee22ce2aba4877ab8f12e22b304
simonliuyuwei_apmic <simon3458@gmail.com>
2025-10-24 00:15:36 +0800

    fix/Remove unusable folder

--- test/my-new-agent/my-new-agent/agent.py
+++
@@ -1,9 +0,0 @@
-from google.adk.agents import Agent
-from google.adk.tools import google_search
-
-root_agent = Agent(
-    name="my-new-agent",
-    model="gemini-1.5-flash",
-    instruction="You are a helpful assistant.",
-    tools=[google_search]
-)
